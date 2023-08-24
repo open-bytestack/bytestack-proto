13f6d34d60ca066c980faaa544bd5d2f316983ae
dashjay <dashjwz@gmail.com>
2023-08-24 16:24:05 +0800

    update

--- format/v1/v1.rs
+++ format/v1/v1.rs
@@ -8,7 +8,7 @@ pub struct MagicHeader {
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct DataHeader {
+pub struct DataRecordHeader {
     #[prost(uint32, tag = "1")]
     pub magic_start: u32,
     #[prost(uint32, tag = "2")]
@@ -23,16 +23,16 @@ pub struct DataHeader {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct IndexRecord {
-    #[prost(uint32, tag = "1")]
-    pub cookie: u32,
-    #[prost(uint64, tag = "2")]
+    #[prost(uint64, tag = "1")]
     pub offset_data: u64,
-    #[prost(uint64, tag = "3")]
-    pub size_data: u64,
+    #[prost(uint32, tag = "2")]
+    pub size_data: u32,
+    #[prost(uint32, tag = "3")]
+    pub cookie: u32,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct MegicRecord {
+pub struct MetaRecord {
     #[prost(int64, tag = "1")]
     pub create_time: i64,
     #[prost(uint64, tag = "2")]
@@ -45,4 +45,36 @@ pub struct MegicRecord {
     pub filename: ::prost::alloc::string::String,
     #[prost(bytes = "vec", tag = "6")]
     pub extra: ::prost::alloc::vec::Vec<u8>,
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum MagicNumber {
+    None = 0,
+    HeaderMagic = 199809,
+    DataMagicStart = 257758,
+    DataMagicEnd = 857752,
+}
+impl MagicNumber {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            MagicNumber::None => "None",
+            MagicNumber::HeaderMagic => "HeaderMagic",
+            MagicNumber::DataMagicStart => "DataMagicStart",
+            MagicNumber::DataMagicEnd => "DataMagicEnd",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "None" => Some(Self::None),
+            "HeaderMagic" => Some(Self::HeaderMagic),
+            "DataMagicStart" => Some(Self::DataMagicStart),
+            "DataMagicEnd" => Some(Self::DataMagicEnd),
+            _ => None,
+        }
+    }
 }
